c9d41302e724c6c56e2afa44e25a53a305ccd3c4
PaulSoft | Torsten Ulrich <tul@paulsoft2.local>
2019-04-24 17:35:59 +0200

    tools

---
+++ js/tools.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+  $(".nav-tabs a").click(function(){
+    $(this).tab('show');
+  });
+  $('.nav-tabs a').on('shown.bs.tab', function(event){
+    var x = $(event.target).text();         // active tab
+    var y = $(event.relatedTarget).text();  // previous tab
+    $(".act span").text(x);
+    $(".prev span").text(y);
+  });
+
+    $.getJSON("https://jsonip.com?callback=?", function(data) {
+       document.getElementById("p-ip").innerHTML = data.ip;
+    });
+  
+
+    getUserIP(function(ip){
+        document.getElementById("ip").innerHTML = ip ;
+    });
+
+    length=1;
+    $("#input").focusin(function (evt) {
+       
+        $(this).keypress(function () {
+            content=$(this).val();
+            content1 = content.replace(/\:/g, '');
+            length=content1.length;
+            if(((length % 2) == 0) && length < 10 && length > 1){
+                $('#input').val($('#input').val() + ':');
+                }
+    
+        });
+    
+    });
+
+
+});
+
+
+/**
+ * Get the user IP throught the webkitRTCPeerConnection
+ * @param onNewIP {Function} listener function to expose the IP locally
+ * @return undefined
+ */
+function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+    //compatibility for firefox and chrome
+    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+    var pc = new myPeerConnection({
+        iceServers: []
+    }),
+    noop = function() {},
+    localIPs = {},
+    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+    key;
+
+    function iterateIP(ip) {
+        if (!localIPs[ip]) onNewIP(ip);
+        localIPs[ip] = true;
+    }
+
+     //create a bogus data channel
+    pc.createDataChannel("");
+
+    // create offer and set local description
+    pc.createOffer(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+            if (line.indexOf('candidate') < 0) return;
+            line.match(ipRegex).forEach(iterateIP);
+        });
+        
+        pc.setLocalDescription(sdp, noop, noop);
+    }, noop); 
+
+    //listen for candidate events
+    pc.onicecandidate = function(ice) {
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+    };
+}
+
+
+
